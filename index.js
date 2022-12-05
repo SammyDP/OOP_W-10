fc8cd8b23477718ae25f9d7110146270f0908692
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-05 08:58:25 -0700

    manager part created
    still need to do engineer and intern, then have it render to page

--- index.js
+++ index.js
@@ -4,4 +4,27 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const questions = {};
+const questions = {
+  Manager: [
+    {
+      type: "input",
+      message: "What is the manager's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the manager's ID number?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the manager's email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the manager's office number?",
+      name: "office",
+    },
+  ],
+};
